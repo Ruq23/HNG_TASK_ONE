39cf96569c341f0735a5d910c2658b6d3684f6f6
“Ruq23” <daruq20@gmail.com>
2022-10-27 19:13:08 +0100

    fix

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express();
 const path = require('path')
+const cors = require('cors')
 
 
 
 app.use(express.urlencoded({extended: true}));
+app.use(cors({
+    origin: '*'
+}));
 
 app.get('/', (req, res) => {
     const age = '22'
