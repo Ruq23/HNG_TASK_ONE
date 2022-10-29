e4cee5f68393fef0c33aaf25b74f3cb3ef653781
“Ruq23” <daruq20@gmail.com>
2022-10-29 17:34:23 +0100

    fix

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors({
 app.get('/', (req, res) => {
     const age = '22'
     const bio = "I am a hardworking, articulate and ambitious individual, i am passionate about Web Development and i am excited to join the HNG Team."
-    const slackUsername = "Faruq"
+    const slackUsername = "Faruq22"
     const backend = true
 
     res.send(`{"slackUsername": "${slackUsername}", "backend": ${backend}, "age": ${age}, "bio": "${bio}"}`)
