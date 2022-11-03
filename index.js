01c8d6bd223c7191956255a72d6448436d2d362e
“Ruq23” <daruq20@gmail.com>
2022-11-03 17:45:32 +0100

    task two

--- index.js
+++ index.js
@@ -2,18 +2,25 @@ const express = require('express')
 const app = express();
 const path = require('path')
 const cors = require('cors')
+const methodOverride = require('method-override')
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 
 app.use(express.urlencoded({extended: true}));
-app.use(cors({
-    origin: '*'
-}));
+app.use(methodOverride('_method'))
 
-app.use(function(req, res, next) {
-    res.setHeader("Content-Type", "application/json");
-    next();
-});
+// app.use(cors({
+//     origin: '*'
+// }));
+
+// app.use(function(req, res, next) {
+//     res.setHeader("Content-Type", "application/json");
+//     next();
+// });
+
+const operation_type = ['addition', 'subtraction', 'multiplication', 'division']
 
 app.get('/', (req, res) => {
     const age = 22
@@ -24,6 +31,29 @@ app.get('/', (req, res) => {
     res.send(`{"slackUsername": "${slackUsername}", "backend": ${backend}, "age": ${age}, "bio": "${bio}"}`)
 })
 
+
+
+app.get('/calculator', (req, res) => {
+    res.render('calculator.ejs', { operation_type })
+})
+app.post('/calculator', (req, res) => {
+    const slackUsername = "Faruq22"
+    const { xinput, yinput, operation_type } = req.body
+    const x = (parseInt(xinput))
+    const y = (parseInt(yinput))
+    // console.log(typeof realx)
+    if(operation_type == 'addition') {
+        res.send(`{"slackUsername": "${slackUsername}", "result": ${x + y}, "operation_type: ${operation_type}}`)
+    }if(operation_type == 'subtraction') {
+        res.send(`{"slackUsername": "${slackUsername}", "result": ${x - y}, "operation_type: ${operation_type}}`)
+    }if(operation_type == 'multiplication') {
+        res.send(`{"slackUsername": "${slackUsername}", "result": ${x * y}, "operation_type: ${operation_type}}`)
+    }if(operation_type == 'division') {
+        res.send(`{"slackUsername": "${slackUsername}", "result": ${x / y}, "operation_type: ${operation_type}}`)
+    }
+})
+
 app.listen(3000, () => {
     console.log('Listening on Port 3000')
 })
+
