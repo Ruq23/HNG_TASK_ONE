283e7832f6cbf69c37c97478d3177f187967afa0
“Ruq23” <daruq20@gmail.com>
2022-11-05 12:51:05 +0100

    fix

--- index.js
+++ index.js
@@ -9,19 +9,19 @@ const methodOverride = require('method-override');
 
 
 
-app.use(express.urlencoded({extended: true}));
+// app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'))
 
-// app.use(cors({
-//     origin: '*'
-// }));
+app.use(express.json());
+app.use(cors({
+    origin: '*'
+}));
 
-// app.use(function(req, res, next) {
-//     res.setHeader("Content-Type", "application/json");
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.setHeader("Content-Type", "application/json");
+    next();
+});
 
-const operation_type = ['addition', 'subtraction', 'multiplication', 'division']
 
 app.get('/', (req, res) => {
     const age = 22
@@ -56,12 +56,12 @@ app.get('/', (req, res) => {
             result = null;
           }
       
-        //   res.status(200).json({
-        //     slackUsername: "Faruq22",
-        //     result,
-        //     operation_type: details.operation_type,
-        //   });
-          res.send(`{"slackUsername": "${"Faruq22"}", "result": ${result}, "operation_type: ${details.operation_type}}`)
+          res.status(200).json({
+            slackUsername: "Faruq22",
+            result,
+            operation_type: details.operation_type,
+          });
+        //   res.send(`{"slackUsername": "${"Faruq22"}", "result": ${result}, "operation_type: ${details.operation_type}}`)
         } catch (error) {
           console.log(error);
           res.status(404).json(error);
